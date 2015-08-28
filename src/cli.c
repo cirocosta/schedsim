dc4c422a31f87da14c95332024d483432073481f
Ciro S. Costa <ciro9758@gmail.com>
2015-08-28 01:11:51 -0300

    separates cli declarations and definitions

---
+++ src/cli.c
@@ -0,0 +1,37 @@
+#include "schedsim/cli.h"
+
+void sm_cli_help()
+{
+  fprintf(stderr, "%s\n", SM_CLI_HELP_TXT);
+}
+
+sm_cli_args_t* sm_cli_parse(const int argc, const char** argv)
+{
+  sm_cli_args_t* args = NULL;
+
+  if (argc < 4) {
+    sm_cli_help();
+    fprintf(stderr, "%s\n", "Error: Not enough arguments provided.");
+    exit(EXIT_FAILURE);
+  }
+
+  args = malloc(sizeof(*args));
+  args->sched_alg = (enum sm_sched_algorithms)strtoul(argv[1], NULL, 10);
+
+  if (args->sched_alg > SM_RT_RIGID_READLINES ||
+      args->sched_alg < SM_FIRSTCOME_FIRSTSERVED) {
+    sm_cli_help();
+    fprintf(stderr, "%s\n", "Error: invalid scheduling algorithm number.");
+    exit(EXIT_FAILURE);
+  }
+
+  strncpy(args->in_fname, argv[2], PATH_MAX);
+  strncpy(args->out_fname, argv[3], PATH_MAX);
+  args->debug = argc == 5;
+
+  return args;
+}
+
+void sm_cli_parse_input(const char* filename)
+{
+}
