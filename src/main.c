62d2ee3581b8edf1532654183edf7501a87ad99f
Ciro S. Costa <ciro9758@gmail.com>
2015-09-11 03:17:24 -0300

    starts priority sched and improves main

--- src/main.c
+++ src/main.c
@@ -1,38 +1,57 @@
 #include "schedsim/common.h"
+#include "schedsim/cli.h"
+#include "schedsim/scheduler.h"
+
+#include "schedsim/schedulers/firstcome_firstserved.h"
+#include "schedsim/schedulers/shortest_job_first.h"
+#include "schedsim/schedulers/shortest_remaining_time_next.h"
+#include "schedsim/schedulers/round_robin.h"
+#include "schedsim/schedulers/priority_sched.h"
 
 #include <time.h>
 #include <stdio.h>
 
 int main(const int argc, const char* argv[])
 {
+  unsigned i = 0;
+  FILE* fp;
+
 #ifndef NDEBUG
   setbuf(stdout, NULL);
 #endif
-  /* sm_sched_firstcome_firstserved(NULL, 0); */
-
-#if 0
   sm_cli_args_t* args = sm_cli_parse(argc, argv);
   size_t traces_count;
   sm_trace_t** traces = sm_get_traces(args->in_fname, &traces_count);
 
   switch (args->sched_alg) {
     case SM_FIRSTCOME_FIRSTSERVED:
-      sm_sched_firstcome_firstserved(traces);
-      break;
+      sm_sched_firstcome_firstserved(traces, traces_count);
     case SM_S_JOB_FIRST:
-      break;
+      sm_sched_shortest_job_first(traces, traces_count);
     case SM_S_REMAINING_TIME_NEXT:
-      break;
+      sm_sched_shortest_remaining_time_next(traces, traces_count);
     case SM_ROUND_ROBIN:
-      break;
+      sm_sched_round_robin(traces, traces_count);
     case SM_SCHED_WITH_PRIORITY:
-      break;
+      LOGERR("Unsupported SCHED WITH PRIORITY");
+    /* sm_sched_priority_sched(traces, traces_count); */
     case SM_RT_RIGID_DEADLINES:
+      LOGERR("Unsupported RIGIT DEADLINES");
       break;
   }
 
+  fp = fopen(args->out_fname, "w");
+  ASSERT(fp, "Couldn't open file `%s` for write.", args->out_fname);
+
+  for (; i < traces_count; i++) {
+    fprintf(fp, "%s %f %f\n", traces[i]->pname, traces[i]->out.tf / 1e6,
+            traces[i]->out.tr / 1e6);
+  }
+  fprintf(fp, "%u", 10);
+
+  fclose(fp);
   FREE(args);
   FREE_ARR(traces, traces_count);
-#endif
+
   return 0;
 }
--- tests/test-priority-sched.c
+++ tests/test-priority-sched.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#include "schedsim/schedulers/round_robin.h"
+#include "schedsim/schedulers/priority_sched.h"
 #include "schedsim/trace.h"
 #include "schedsim/common.h"
 
@@ -14,7 +14,7 @@ void test2()
     sm_parse_trace("1.0 process3 20 20"),
   };
 
-  sm_sched_round_robin(traces, traces_size);
+  sm_sched_priority_sched(traces, traces_size);
 
   while (traces_size --> 0)
     sm_trace_destroy(traces[traces_size]);
@@ -34,7 +34,7 @@ void test1()
     sm_parse_trace("1.0 process8 3 20"),
   };
 
-  sm_sched_round_robin(traces, traces_size);
+  sm_sched_priority_sched(traces, traces_size);
 
   while (traces_size --> 0)
     sm_trace_destroy(traces[traces_size]);
@@ -42,8 +42,8 @@ void test1()
 
 int main(int argc, char* argv[])
 {
-  TEST(test1);
-  TEST(test2);
+  /* TEST(test1); */
+  /* TEST(test2); */
 
   return 0;
 }
